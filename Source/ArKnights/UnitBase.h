7708cffccf0eab5935001bb495e4aeae0b0eb973
KimJiHo <shield1203@naver.com>
2020-11-24 23:14:52 +0900

    작전시작전 부대편성, UnitBase상태 enum

--- Source/ArKnights/UnitBase.h
+++ Source/ArKnights/UnitBase.h
@@ -4,13 +4,40 @@
 #include "PaperCharacter.h"
 #include "UnitBase.generated.h"
 
+UENUM(BlueprintType)
+enum class EUnitState : uint8
+{
+	Start UMETA(DisplayName = "Unit_Start"),
+	Idle UMETA(DisplayName = "Unit_Idle"),
+	Move UMETA(DisplayName = "Unit_Move"),
+	Attack UMETA(DisplayName = "Unit_Attack"),
+	Skill UMETA(DisplayName = "Unit_Skill"),
+	Die UMETA(DisplayName = "Unit_Die"),
+};
+
+UENUM(BlueprintType)
+enum class ERangeDerection : uint8
+{
+	North UMETA(DisplayName = "North"),
+	South UMETA(DisplayName = "South"),
+	West UMETA(DisplayName = "West"),
+	East UMETA(DisplayName = "East"),
+};
+
+UENUM(BlueprintType)
+enum class EUnitForm : uint8
+{
+	Front UMETA(DisplayName = "Front"),
+	Back UMETA(DisplayName = "Back"),
+};
+
 UCLASS(Blueprintable, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class ARKNIGHTS_API AUnitBase : public APaperCharacter
 {
 	GENERATED_BODY()
 	
 protected:
-
+	bool m_flip;
 
 public:
 	AUnitBase();
